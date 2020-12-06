d254196dc7cde53057e6f4ba3138d4edf61d0d89
Samicap <lovestochasewindmills@gmail.com>
2020-12-06 00:32:31 +0000

    updated code to iss.js

--- iss.js
+++ iss.js
@@ -1,10 +1,23 @@
 
 const request = require('request');
 
+const searchURLForIP = 'https://api.ipify.org?format=json';
+
 
 const fetchMyIP = function(callback) {
   
-  request(URL, (error, response, body) => {
+  request(searchURLForIP, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+    if (body !== "[]") {
+      let data = JSON.parse(body);
+      description = data.ip;
+      console.log(description)
+    }
 
   });
 };
